b574a50cdb94f3a3d674a215f76fd9beb7f6a669
João Pedro Lima <joaopedrocal90@gmail.com>
2025-12-13 11:32:40 -0300

    application done

--- services/api.ts
+++ services/api.ts
@@ -1,14 +1,12 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-// const LOCAL_HOME_IP = "192.168.15.8";
-// const LOCAL_UNIFOR_INSTITUCIONAL_IP = "10.81.16.233";
-const LOCAL_PHONE_IP = "172.20.10.4";
-
 const getBaseUrl = () => {
-  if (process.env.EXPO_PUBLIC_API_URL) return process.env.EXPO_PUBLIC_API_URL;
+  if (process.env.EXPO_PUBLIC_API_URL) {
+    return process.env.EXPO_PUBLIC_API_URL;
+  }
 
-  return `http://${LOCAL_PHONE_IP}:3000/v1/api`;
+  throw new Error("No API url provided.");
 };
 
 export const api = axios.create({
